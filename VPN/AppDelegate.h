95c76ffb3cd06d7944c48411a4eadd9546d2820d
xiaoanning <734871221@qq.com>
2017-05-31 10:53:13 +0800

    Initial Commit

---
+++ VPN/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VPN
+//
+//  Created by 安宁 on 2017/5/31.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VPN/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VPN
+//
+//  Created by 安宁 on 2017/5/31.
+//  Copyright © 2017年 安宁. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
